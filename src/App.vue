7f1e42c4540bd2e0636c0129a4adfc89a0fe5194
Florian Zug <florian.zug@auvesy.de>
2020-11-06 17:41:25 +0100

    almost done

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="brown"
+      dark
+    >
+      <v-spacer></v-spacer>
+        <h2> Raubzug-Rechner </h2>
+
+
+      <v-spacer></v-spacer>
+
+      
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
